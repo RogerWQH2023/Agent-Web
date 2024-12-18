81fe8f844f5d7b60c32be22a5ed2bb7ce74b9186
RogerWQH2023 <rogerzju2021@gmail.com>
2024-12-18 10:36:29 +0800

    feat: 初始化chat界面

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,9 +2,11 @@ import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import Workflow from "@/pages/Workflow/index.vue";
 // import { default as ChatIndex } from "@/pages/Chat/index.vue";
 import Home from "@/pages/Home/index.vue"
+import Chat from "@/pages/Chat/index.vue"
 
 const routes: RouteRecordRaw[] = [
   { path: "/", component: Home },
+  { path: "/chat", component: Chat },
   { path: "/workflow", component: Workflow },
   // { path: "/chat", component: ChatIndex },
 ];
