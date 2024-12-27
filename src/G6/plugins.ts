413f10a6b1b546b39adadceaf1f5eb8f6b4783bf
RogerWQH2023 <rogerzju2021@gmail.com>
2024-12-27 21:14:10 +0800

    perf: 优化tooltip样式

--- src/G6/plugins.ts
+++ src/G6/plugins.ts
@@ -2,14 +2,14 @@ import G6 from "@antv/g6";
 import { Task } from "@/type.ts";
 
 export const tooltip = new G6.Tooltip({
+  className: "g6-custom-tooltip",
+  /*   container: document.body, */
+  /* fixToNode: [1, 0.5], */
   offsetX: 10,
   offsetY: 20,
   getContent(e) {
-    const outDiv = document.createElement("div");
-    outDiv.style.width = "180px";
-    outDiv.innerHTML = `
-      <p>${(e?.item?.getModel().task as Task).name}</p>
-    `;
+    const outDiv = document.createElement("p");
+    outDiv.innerHTML = `${(e?.item?.getModel().task as Task).name}`;
     return outDiv;
   },
   itemTypes: ["node"],
