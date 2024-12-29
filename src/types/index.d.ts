11d904545401051964649079bf21a7e9b4828197
RogerWQH2023 <rogerzju2021@gmail.com>
2024-12-29 21:51:54 +0800

    chore: 更新类型

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,1 +1,26 @@
 export type AgentType = "chat" | "workflow";
+
+export type GISQAMessage<T extends AgentType> = GISQAMessageType[T];
+
+export type GISQAMessageType = {
+  "chat": GISQAChatMessage;
+  "workflow": GISQAWorkflowMessage;
+}
+export type GISQAChatMessage = {
+  role: "user" | "assistant",
+  content: string
+}
+export type GISQAWorkflowMessage = {
+  role: "user",
+  content: string
+} | {
+  role: "assistant",
+  content: OriTask
+}
+
+export interface OriTask {
+  task: string;
+  toolId: string;
+  toolName: string;
+  subtasks: Array<OriTask>;
+}
