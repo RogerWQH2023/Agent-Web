1b76e0552374569b351c540c11af4528792024e6
RogerWQH2023 <rogerzju2021@gmail.com>
2024-12-29 21:47:39 +0800

    feat: 更改tooltip样式

--- src/G6/plugins.ts
+++ src/G6/plugins.ts
@@ -4,13 +4,11 @@ import { Task } from "@/type.ts";
 export const tooltip = new G6.Tooltip({
   className: "g6-custom-tooltip",
   /*   container: document.body, */
-  /* fixToNode: [1, 0.5], */
+  fixToNode: [1, 0.5],
   offsetX: 10,
   offsetY: 20,
   getContent(e) {
-    const outDiv = document.createElement("p");
-    outDiv.innerHTML = `${(e?.item?.getModel().task as Task).name}`;
-    return outDiv;
+    return `${(e?.item?.getModel().task as Task).name}`;
   },
   itemTypes: ["node"],
 });
